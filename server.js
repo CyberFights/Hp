0761d2b76de565e43e7b111ceedef0a3378daf04
CyberFights <shadowjer011@gmail.com>
2026-04-13 19:31:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -293,7 +293,7 @@ app.post("/api/game-state", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 });
